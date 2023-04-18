7615b477a1d9e50ae75bcb6898f46a4b16e484dd
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-04-18 13:01:35 +0200

    Add Second Message

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,8 @@ const { createApp } = Vue
     data() {
       return {
         message: 'Hello Vue!',
-        image: "https://cdn1.dotesports.com/wp-content/uploads/2019/09/03101714/Garen_13.jpg"
+        message2: 'This is Garen, the best champ of the League of Legends!',
+        image: 'https://cdn1.dotesports.com/wp-content/uploads/2019/09/03101714/Garen_13.jpg'
       }
     }
   }).mount('#app')
