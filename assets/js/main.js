f0e32cd18dfebc096f9a3741f8c0a5fd4830cb95
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-04-18 12:52:16 +0200

    Completamento esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        image: "https://cdn1.dotesports.com/wp-content/uploads/2019/09/03101714/Garen_13.jpg"
       }
     }
   }).mount('#app')
